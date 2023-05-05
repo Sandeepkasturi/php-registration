91b25105d7385becbbd25d87e1388b299e8300c6
Sandeepkasturi <112337452+Sandeepkasturi@users.noreply.github.com>
2023-05-05 19:32:56 +0530

    redirect code

---
+++ registration/redirect.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <style>
+        body {
+            background-color: #f5f5f5;
+            font-family: Arial, sans-serif;
+        }
+ marquee {
+            text-align: center;
+            padding: 20px;
+            margin: 50px auto;
+            color: #fff;
+            background-color: #3498db;
+            border-radius: 10px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+            font-size: 36px;
+            font-weight: bold;
+            text-transform: uppercase;
+            letter-spacing: 2px;
+        }
+
+        h1 {
+            text-align: center;
+            padding: 20px;
+            margin: 50px auto;
+            color: #fff;
+            background-color: #3498db;
+            border-radius: 10px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+            font-size: 36px;
+            font-weight: bold;
+            text-transform: uppercase;
+            letter-spacing: 2px;
+        }
+    </style>
+</head>
+<body>
+<marquee direction="right" align="center" scroll="8"><big>redeemtechai.vercel.app</big></marquee>
+    <h1>Thanks for submitting your data.</h1></br>
+    <h1>You Believe in Our Service. We make sure your Security and Privacy way Better</h1>
+</body>
+</html>
